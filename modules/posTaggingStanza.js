8e4ad221030f47ed8cf370e53885d57510eda6bf
prajadimas <dmspraja2105@gmail.com>
2021-01-10 15:54:03 +0700

    postag url

--- modules/posTaggingStanza.js
+++ modules/posTaggingStanza.js
@@ -12,8 +12,8 @@ module.exports = function posTaggingStanza(sentence) {
 // function posTaggingStanza(sentence) {
   return new Promise(async (resolve, reject) => {
     try {
-      // var results = await axios.get('http://103.122.5.98:5000/api/postag?s=' + encodeURIComponent(sentence))
-      var results = await axios.get('http://127.0.0.1:5000/api/postag?s=' + encodeURIComponent(sentence))
+      var results = await axios.get('http://103.122.5.98:5000/api/postag?s=' + encodeURIComponent(sentence))
+      // var results = await axios.get('http://127.0.0.1:5000/api/postag?s=' + encodeURIComponent(sentence))
       console.log('Proses POSTagger Stanza: ', results.data)
       resolve(results.data)
     } catch (err) {
